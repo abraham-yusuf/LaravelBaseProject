418ae48d9b6db0cc02ec6c69a7e9fe415c6f69d9
cronycles <cronycles@gmail.com>
2018-09-22 13:25:26 +0200

    messo app.min.js

--- LaravelBaseProject/resources/views/welcome.blade.php
+++ LaravelBaseProject/resources/views/welcome.blade.php
@@ -57,6 +57,10 @@
     </div>
 </div>
 <script src="{{ hashed('js/vendors.min.js') }}" defer></script>
-<script src="{{ hashed('js/app.js') }}" defer></script>
+@if (config('app.env') == 'production')
+    <script src="{{ hashed('js/app.js') }}" defer></script>
+@else
+    <script src="{{ hashed('js/app.min.js') }}" defer></script>
+@endif
 </body>
 </html>
