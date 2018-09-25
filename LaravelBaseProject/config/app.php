55b424d9f133dc5630c48d537fa37262f9adb7c5
cronycles <cronycles@gmail.com>
2018-09-25 20:06:17 +0200

    changing release version to: v0.2

--- LaravelBaseProject/config/app.php
+++ LaravelBaseProject/config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'LaravelBaseProject'),
 
-    'version' => env('APP_VERSION', '0.1'),
+    'version' => env('APP_VERSION', '0.2'),
 
     /*
     |--------------------------------------------------------------------------
