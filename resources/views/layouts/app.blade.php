d030a2ff326f150e7e469e06629a8d191c65d7ae
cronycles <cronycles@gmail.com>
2020-05-10 16:40:22 +0200

    google analytics aggiunto!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,16 +3,17 @@
 @include('custom.layouts.head._head')
 
 <body>
-    @include('custom.layouts._client-server')
-    @render(\App\Http\ViewComponents\Header\Components\HeaderComponent::class)
+@include('custom.layouts._client-server')
+@render(\App\Http\ViewComponents\Header\Components\HeaderComponent::class)
 
-    <div class="jpage wrapper" data-p="{{$model->id}}">
-        @include('custom.form.messages.success')
-        @include('layouts.partials._breadcrumbs', ['breadcrumbs' => $model->breadcrumbs])
+<div class="jpage wrapper" data-p="{{$model->id}}">
+    @include('custom.form.messages.success')
+    @include('layouts.partials._breadcrumbs', ['breadcrumbs' => $model->breadcrumbs])
 
-         @yield('content')
-    </div>
-    @include('custom.layouts._scripts')
+    @yield('content')
+</div>
+@include('custom.layouts._scripts')
+@include('custom.layouts._analytics')
 </body>
 
 </html>
