b0b4225795a37e1dbbc5628daccc52272d2c2a85
cronycles <cronycles@gmail.com>
2020-05-12 19:09:56 +0200

    fix testi

--- resources/lang/it/images-upload.php
+++ resources/lang/it/images-upload.php
@@ -2,8 +2,10 @@
 
 return [
     'select-images-button' => 'Carica nuove immagini',
-    'drag-drop-images' => 'O trascina le immagini qui dentro...',
-    'thumbs-description' => 'Trascina le immagini per ordinarle o clicca sul cestino per eliminarle',
+    'drag-drop-images' => 'O trascina le nuove immagini qui dentro...',
+    'thumbs-description' => 'Se vuoi cambiare l\'oridne di una immagine, clicca sul bottone dell due frecce e trascina
+     l\'immagine nella posizione che desideri. Se desideri eliminarla fai click sull\'icona del cestino. Altrimenti se
+     vuoi settare la small view, clicca sull\'icona delle frecce che convergono.',
     'sort-image-button' => 'Ordina',
     'small-image-button' => 'Small view',
     'delete-image-button' => 'Elimina',
