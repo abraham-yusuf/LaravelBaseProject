8a8a1f3a1e99446f6eac8c252d249ddcc8035aab
cronycles <cronycles@gmail.com>
2020-05-14 18:32:40 +0200

    company name

--- config/custom/company.php
+++ config/custom/company.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name' => 'Crosetti Interior Design',
+    'name' => 'cro\' . . .in the morning',
     'vat-number' => '00246870109',
     'address' => 'Via L. Vetrano 37r, 16152 Genova',
     'telephone' => '0106507227',
-    'email' => 'info@crosetti.it',
+    'email' => 'info@crointhemorning.com',
     'google-maps-url' => 'https://goo.gl/maps/atgEdtEK3Vu'
 ];
