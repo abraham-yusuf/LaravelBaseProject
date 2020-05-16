ed8c01d458c2fb1a25789d632213e6f068304070
cronycles <cronycles@gmail.com>
2020-05-16 22:27:06 +0200

    fix cache

--- config/custom/cache.php
+++ config/custom/cache.php
@@ -1,8 +1,13 @@
 <?php
 
 return [
+
+    'isEnabled' => env('CACHE_ENABLED'),
+
     'api' => [
         'users' => ['key' => 'users', 'seconds' => 60],
-        'languages' => ['key' => 'languages', 'seconds' => 60]
+        'languages' => ['key' => 'languages', 'seconds' => 60],
+        'categories' => ['key' => 'categories', 'seconds' => 60],
+        'projects' => ['key' => 'projects', 'seconds' => 60],
     ]
 ];
