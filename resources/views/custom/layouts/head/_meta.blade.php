56176dd8687a9fc0f8dd5627b613993ff62aa9fe
cronycles <cronycles@gmail.com>
2020-05-17 17:45:43 +0200

    favicon!

--- resources/views/custom/layouts/head/_meta.blade.php
+++ resources/views/custom/layouts/head/_meta.blade.php
@@ -3,9 +3,5 @@
 <meta name="description" content="{{$model->htmlMetaDescription}}"/>
 <meta name="keywords" content="{{$model->htmlMetaKeywords}}">
 <meta name="author" content="cronycles">
-<meta property="og:title" content="{{$model->htmlTitle}}">
-<meta property="og:description" content="{{$model->htmlMetaDescription}}" />
-<meta property="og:image" content="{{$model->ogImageUrl}}" />
-<meta property="og:type" content="website" />
-<meta property="og:locale" content="{{$model->currentLanguage->cultureCode}}" />
-<meta name="csrf-token" content="{{csrf_token()}}" />
+@include('custom.layouts.head._meta-favicon')
+@include('custom.layouts.head._meta-og')
